8dd69a02d34ad26644c02ed6bbcad7641c245790
Marco Pinto <marco.alp120@gmail.com>
2022-07-18 21:08:46 +0100

    Added memory widget
    - Memory icon widget added;

--- qtile_widgets/__init__.py
+++ qtile_widgets/__init__.py
@@ -1,4 +1,5 @@
 from .battery_icon import BatteryIcon
 from .brightness_icon import BrightnessIcon
 from .cpu_icon import CPUIcon
+from .memory_icon import MemoryIcon
 from .volume_icon import VolumeIcon
---
+++ qtile_widgets/memory_icon.py
@@ -0,0 +1,58 @@
+from libqtile.widget import base
+import psutil
+
+from .round_progress_bar import RoundProgressBar
+
+
+class MemoryIcon(RoundProgressBar):
+    defaults = [
+        ("timeout", 1, "How often in seconds the widget refreshes."),
+        ("icons", [
+            ((0, 100), "\uf85a"),
+        ], "Icons to present inside progress bar, based on progress thresholds."),
+        ("thresholds", [
+            ((0, 50), ("00ff00", "")),
+            ((50, 75), ("ffff00", "")),
+            ((75, 100), ("ff0000", "")),
+        ], "Defines different colors for each specified threshold.")
+    ]
+
+    def __init__(self, **config):
+        super().__init__(**config)
+        self.add_defaults(MemoryIcon.defaults)
+        self.add_defaults(base._TextBox.defaults)
+        self.update_level()
+
+    def _configure(self, qtile, bar):
+        super()._configure(qtile, bar)
+        if self.timeout:
+            self.timeout_add(self.timeout, self.loop)
+
+    def get_icon(self):
+        for threshs, icon in self.icons:
+            if self._level >= threshs[0] and self._level <= threshs[1]:
+                return icon
+        return ""
+
+    def update_level(self):
+        memory = psutil.virtual_memory()
+        self._level = round((memory.used / memory.total) * 100, 1)
+
+    def update(self):
+        self.update_level()
+        self.draw()
+
+    def loop(self):
+        self.update()
+        self.timeout_add(self.timeout, self.loop)
+
+    def draw(self):
+        self.drawer.clear(self.background or self.bar.background)
+        # draw progress bar
+        self.draw_progress(self._level)
+        # draw icon
+        icon = self.drawer.textlayout(self.get_icon(), self.foreground, self.font, self.fontsize, None, wrap=False)
+        icon_x = (self.prog_width - icon.width) / 2
+        icon_y = (self.prog_height - icon.height) / 2
+        icon.draw(icon_x, icon_y)
+        self.drawer.draw(offsetx=self.offset, offsety=self.offsety, width=self.length)
