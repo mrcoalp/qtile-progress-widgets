95de18e4fc581e7fb3260faa7329c2a78d59d209
Marco Pinto <marco.alp120@gmail.com>
2022-09-28 18:44:25 +0100

    refactor(logger): update logger message to include date
    Logs now also include day of occurrence.

--- qtile_awesome_widgets/logger.py
+++ qtile_awesome_widgets/logger.py
@@ -1,15 +1,16 @@
 import logging
 from logging.handlers import RotatingFileHandler
 import os
+import site
 
-formatter = logging.Formatter("[%(asctime)s][%(name)s][%(levelname)s]: %(message)s", "%H:%M:%S")
+formatter = logging.Formatter("[%(asctime)s][%(name)s][%(levelname)s]: %(message)s", "%Y/%m/%d %H:%M:%S")
 
-log_dir = os.path.expanduser("~/.local/share/qtile-awesome-widgets")
+log_dir = os.path.join(site.getuserbase(), "share", "qtile-awesome-widgets")
 
 if not os.path.exists(log_dir):
     os.makedirs(log_dir)
 
-handler = RotatingFileHandler(os.path.join(log_dir, "widgets.log"), maxBytes=1024, backupCount=5)
+handler = RotatingFileHandler(os.path.join(log_dir, "widgets.log"), maxBytes=1024 * 1024 * 5, backupCount=5)
 handler.setFormatter(formatter)
 
 
